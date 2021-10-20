22369436b51b4665ec1bfee53bf747b9c86ba6cb
Mounir El-Abbas <eamounir1@gmail.com>
2021-10-20 11:58:22 -0400

    hide API key

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const googleMapsAPIKey = process.env.REACT_APP_GOOGLEMAPS_API_KEY;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -32,3 +33,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+/* 
+https://maps.googleapis.com/maps/api/staticmap?center=Venice,IT&zoom=5&size=400x400&markers=color:red|Venice,IT&key=AIzaSyChtMCSnNftrR0pzqOX-SU7-OEp7BYEmKw
+
+https://developers.google.com/maps/documentation/maps-static/overview?hl=en_GB
+*/
